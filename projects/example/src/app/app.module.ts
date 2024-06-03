d252757d84b4a46171f5381ae9d4bd5388d1a218
Gabriel Allaigre <gabriel.allaigre@gmail.com>
2024-06-03 11:35:11 +0200

    Update angular 18

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -7,7 +7,7 @@ import localeFr from '@angular/common/locales/fr';
 import localeEn from '@angular/common/locales/en';
 import { EasyI18nLoader, EasyI18nModule } from '@ngx-easy-i18n-js/core';
 import { EasyI18nBootstrapComponent, EasyI18nBootstrapModule } from '@ngx-easy-i18n-js/bootstrap';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpEasyI18nLoader } from '../../../http-loader/src/lib/http-easy-i18n.loader';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -16,41 +16,37 @@ export const LANGUAGES: { code: string; ngLocale: any; flag: string; name: strin
   { code: 'en', ngLocale: localeEn, flag: 'gb', name: 'English' }
 ];
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    EasyI18nModule.forRoot({
-      options: {
-        logging: false
-      },
-      ngLocales: LANGUAGES.reduce((acc, x) => {
-        acc[x.code] = x.ngLocale;
-        return acc;
-      }, {} as { [key: string]: any; }),
-      defaultLanguage: 'fr',
-      fallbackLanguage: 'en',
-      discover: 'minimum'
-    }),
-    EasyI18nBootstrapModule.forRoot({
-      bootstrap: AppComponent
-    }),
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    {
-      provide: EasyI18nLoader,
-      deps: [HttpClient],
-      useFactory: (httpClient: HttpClient) => new HttpEasyI18nLoader(httpClient, {
-        prefix: '/assets/i18n/'
-      })
-    }
-  ],
-  bootstrap: [EasyI18nBootstrapComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [EasyI18nBootstrapComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        EasyI18nModule.forRoot({
+            options: {
+                logging: false
+            },
+            ngLocales: LANGUAGES.reduce((acc, x) => {
+                acc[x.code] = x.ngLocale;
+                return acc;
+            }, {} as {
+                [key: string]: any;
+            }),
+            defaultLanguage: 'fr',
+            fallbackLanguage: 'en',
+            discover: 'minimum'
+        }),
+        EasyI18nBootstrapModule.forRoot({
+            bootstrap: AppComponent
+        }),
+        AppRoutingModule], providers: [
+        {
+            provide: EasyI18nLoader,
+            deps: [HttpClient],
+            useFactory: (httpClient: HttpClient) => new HttpEasyI18nLoader(httpClient, {
+                prefix: '/assets/i18n/'
+            })
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {
 }
