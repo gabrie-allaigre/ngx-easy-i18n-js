02026d71c6dbe2eac1953769f917bb9b78aa5b1e
Gabriel Allaigre <gabriel.allaigre@gmail.com>
2024-10-08 16:14:12 +0200

    Fix locale currency fix 1

--- projects/core/src/lib/locale-currency.pipe.ts
+++ projects/core/src/lib/locale-currency.pipe.ts
@@ -1,6 +1,6 @@
-import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 import { EasyI18nService } from './easy-i18n.service';
-import { formatCurrency } from '@angular/common';
+import { formatCurrency, getCurrencySymbol } from '@angular/common';
 
 @Pipe({
   name: 'localeCurrency',
@@ -10,11 +10,22 @@ export class LocaleCurrencyPipe implements PipeTransform {
 
   constructor(
     private easyI18nService: EasyI18nService,
-    @Inject(LOCALE_ID) private localeId: string
+    @Inject(LOCALE_ID) private localeId: string,
+    @Inject(DEFAULT_CURRENCY_CODE) private defaultCurrencyCode: string = 'USD'
   ) {
   }
 
-  transform(value: number, currency: string, currencyCode?: string, digitsInfo?: string): string {
-    return formatCurrency(value, this.easyI18nService.ngLocale ?? this.localeId, currency, currencyCode, digitsInfo);
+  transform(value: number, currencyCode?: string, display: 'symbol' | 'symbol-narrow' | 'code' | string = 'symbol', digitsInfo?: string): string {
+    const locale = this.easyI18nService.ngLocale ?? this.localeId;
+
+    let currency: string = currencyCode ?? this.defaultCurrencyCode;
+    if (display !== 'code') {
+      if (display === 'symbol' || display === 'symbol-narrow') {
+        currency = getCurrencySymbol(currency, display === 'symbol' ? 'wide' : 'narrow', locale);
+      } else {
+        currency = display;
+      }
+    }
+    return formatCurrency(value, locale, currency, currencyCode, digitsInfo);
   }
 }
